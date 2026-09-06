0c3137b3a1350ecfc612a0cd606e344185feddb8
jrx100th <jenith.ch15@gmail.com>
2026-09-06 16:57:11 +0530

    Benchmark offline collector replay

--- cmd/aegisgraph/benchmark_test.go
+++ cmd/aegisgraph/benchmark_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
 func BenchmarkDemoSnapshot(b *testing.B) {
 	for i:=0;i<b.N;i++ { _=demoSnapshot("attack-path") }
@@ -10,4 +13,16 @@ func BenchmarkNetworkReachability(b *testing.B) {
 	n:=Node{PublicIP:true,RouteIGW:true,Ingress:[]Ingress{{Protocol:"tcp",FromPort:22,ToPort:22,CIDR:"0.0.0.0/0"}}}
 	b.ResetTimer()
 	for i:=0;i<b.N;i++ { _,_=networkReachable(n) }
+}
+
+func BenchmarkReplayCollection(b *testing.B) {
+	client := makeReplayScenario("internet-sensitive-s3").Client
+	ctx := context.Background()
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i:=0;i<b.N;i++ {
+		if _, err := collectReplay(ctx, client); err != nil {
+			b.Fatal(err)
+		}
+	}
 }
